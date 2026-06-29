40434af4e7fb94874d3afb4e7becbba30f323b9d
Balagurubarann <balagurubaran2002@gmail.com>
2026-06-29 17:23:13 +0530

    Profile: Middleware added

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
-import { show_profile } from "../controllers/user.controller.js"
+import { show_profile } from "../controllers/user.controller.js";
+import { authenticate_user } from "../middleware/verify.js"
 
 const router: Router = Router();
+
+router.use(authenticate_user);
 
 router
     .get('/profile', show_profile)
