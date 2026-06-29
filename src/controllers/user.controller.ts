6862b98f71bf3ede5c3ffdb12577774bbdefa0dd
Balagurubarann <balagurubaran2002@gmail.com>
2026-06-29 17:17:37 +0530

    Profile endpoint initiated

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,10 +1,15 @@
 import type { Request, Response, NextFunction } from "express";
+import db from "../lib/drizzle.js";
+import User from "../db/schema/user.js";
+import { eq } from "drizzle-orm";
 
 async function show_profile(req: Request, res: Response, next: NextFunction) {
 
     try {
 
-        
+        const user = req.user;
+
+        return res.status(200).json(user);
 
     } catch (error) {
         next(error);
